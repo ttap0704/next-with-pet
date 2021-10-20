21de4bd48f3b6e97669896f8d384f771c23d1b26
LeeDaeGyu <ttap0704@naver.com>
2021-10-20 09:44:58 +0900

    set accommodation list

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,9 +2,9 @@ import React from "react";
 import App, { AppContext, AppProps } from "next/app";
 import { Provider } from "react-redux";
 import configureStore from "../reducers/configureStore";
-import '../styles/globals.scss'
+import "../styles/globals.scss";
 
-import Header from '../src/components/header'
+import Header from "../src/components/header";
 
 // store 설정파일 로드
 const store = configureStore();
@@ -13,7 +13,9 @@ const TestApp = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
       <Header />
-      <Component {...pageProps} />
+      <div id="__wrap">
+        <Component {...pageProps} />
+      </div>
     </Provider>
   );
 };
--- pages/accommodation/index.tsx
+++ pages/accommodation/index.tsx
@@ -1,9 +1,20 @@
 // import { useEffect } from "react";
 // import { useDispatch, useSelector } from "react-redux";
 // import { RootState } from "../../reducers";
-import c_style from "../styles/color.module.scss";
+import c_style from "../../styles/color.module.scss";
+import styles from "../../styles/pages/accommodation.module.scss";
 
 const Accommodation = () => {
+  const data = [
+    {
+      title: "수영장 펜션, 반려동물과 함께 즐가세요.",
+      img_path:
+        "https://uploads-ssl.webflow.com/5e5cad32512f4ebf86ae2fa1/5e942f6e5f867827a4659114_mrp_6140-hdr.jpeg",
+      rating: 3.5,
+      review_cnt: 1,
+      location: "화성시 반월동",
+    },
+  ];
   // const { no, text } = useSelector((state: RootState) => state.testReducer);
 
   // const dispatch = useDispatch();
@@ -14,17 +25,33 @@ const Accommodation = () => {
   //   });
   // }, []);
 
-  const addTextHandler = () => {
-    const value = "숙박이라니까";
-
-    return value;
+  const list = () => {
+    return data.map((data) => {
+      return (
+        <div className={styles.list}>
+          <div
+            className={styles.list_img}
+            style={{ backgroundImage: `url(${data.img_path})` }}
+          ></div>
+          <div className={styles.list_text_container}>
+            <div className={styles.list_text}>
+              <h2>{data.title}</h2>
+              <span className={styles.list_rating}>
+                <span className={c_style.text_red}>{data.rating}</span>/5 (
+                {data.review_cnt})
+              </span>
+              <p className={styles.list_location}>{data.location}</p>
+            </div>
+            <div className={styles.list_deco}></div>
+          </div>
+        </div>
+      );
+    });
   };
 
   return (
     <>
-      <div>
-        {addTextHandler()}
-      </div>
+      <div>{list()}</div>
     </>
   );
 };
