a21ce6c217e8820130ee75be2d4e7ba44e1960b0
LeeDaeGyu <ttap0704@naver.com>
2021-11-12 09:55:05 +0900

    set express custom server

---
+++ pages/restaurant/detail_tmp.tsx
@@ -0,0 +1,80 @@
+// import { useEffect } from "react";
+// import { useDispatch, useSelector } from "react-redux";
+// import { RootState } from "../../reducers";
+import color from "../../styles/color.module.scss";
+import styles from "../../styles/pages/restaurant.module.scss";
+
+const Restaurant = () => {
+  // const { no, text } = useSelector((state: RootState) => state.testReducer);
+
+  // const dispatch = useDispatch();
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: RESET_TEXT,
+  //   });
+  // }, []);
+  const data = [
+    {
+      idx: 0,
+      title: "수영장 펜션, 반려동물과 함께 즐가세요.",
+      img_path:
+        "https://uploads-ssl.webflow.com/5e5cad32512f4ebf86ae2fa1/5e942f6e5f867827a4659114_mrp_6140-hdr.jpeg",
+      rating: 3.5,
+      review_cnt: 1,
+      hashtags: "#햄버거 #맛집",
+    },
+    {
+      idx: 1,
+      title: "수영장 펜션, 반려동물과 함께 즐가세요.",
+      img_path:
+        "https://uploads-ssl.webflow.com/5e5cad32512f4ebf86ae2fa1/5e942f6e5f867827a4659114_mrp_6140-hdr.jpeg",
+      rating: 3.5,
+      review_cnt: 1,
+      hashtags: "#햄버거 #맛집",
+    },
+    {
+      idx: 2,
+      title: "수영장 펜션, 반려동물과 함께 즐가세요.",
+      img_path:
+        "https://uploads-ssl.webflow.com/5e5cad32512f4ebf86ae2fa1/5e942f6e5f867827a4659114_mrp_6140-hdr.jpeg",
+      rating: 3.5,
+      review_cnt: 1,
+      hashtags: "#햄버거 #맛집",
+    },
+  ];
+
+  const list = () => {
+    return data.map((data) => {
+      return (
+        <div className={styles.list} key={data.idx}>
+          <div
+            className={styles.list_img}
+            style={{ backgroundImage: `url(${data.img_path})` }}
+          ></div>
+          <div className={styles.list_text_container}>
+            <div className={styles.list_text}>
+              <h2>{data.title}</h2>
+              <span className={styles.list_rating}>
+                <span className={color.text_red}>{data.rating}</span>/5 (
+                {data.review_cnt})
+              </span>
+              <p className={styles.list_hashtags}>{data.hashtags}</p>
+            </div>
+            <div className={styles.list_deco}></div>
+          </div>
+        </div>
+      );
+    });
+  };
+
+  return (
+    <>
+      <div className={styles.restaurant_wrap}>
+        {list()}
+      </div>
+    </>
+  );
+};
+
+export default Restaurant;
